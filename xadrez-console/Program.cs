b89df74c6f48286d24241b4122550cadabe9f26a
Levi Canazzi Junior <levicanazzi@gmail.com>
2022-10-20 22:23:07 -0300

    Criado a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace xadrez_console
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.WriteLine();
         }
     }
 }
